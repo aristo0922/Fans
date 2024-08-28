f77d33651024f0798df2a454d56c1f09d689e9eb
aristo <dkfud2121@naver.com>
2024-08-28 20:23:36 +0900

    feat: init project

---
+++ solo-project-based-on-jypfans/src/main/java/com/jarfans/solo/project/based/on/jypfans/SoloProjectBasedOnJypfansApplication.java
@@ -0,0 +1,13 @@
+package com.jarfans.solo.project.based.on.jypfans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloProjectBasedOnJypfansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloProjectBasedOnJypfansApplication.class, args);
+	}
+
+}
---
+++ solo-project-based-on-jypfans/src/test/java/com/jarfans/solo/project/based/on/jypfans/SoloProjectBasedOnJypfansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarfans.solo.project.based.on.jypfans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloProjectBasedOnJypfansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

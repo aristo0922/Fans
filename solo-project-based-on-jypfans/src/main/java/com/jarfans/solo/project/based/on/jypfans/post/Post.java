e64359321e501216ef23a0aab91a66479f7b36a5
aristo <dkfud2121@naver.com>
2024-08-30 22:53:27 +0900

    feat: post controller에서 postService 호출

--- solo-project-based-on-jypfans/src/main/java/com/jarfans/solo/project/based/on/jypfans/post/Post.java
+++ solo-project-based-on-jypfans/src/main/java/com/jarfans/solo/project/based/on/jypfans/post/Post.java
@@ -1,9 +1,15 @@
 package com.jarfans.solo.project.based.on.jypfans.post;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Builder
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Repository
 public class Post {
   private long id;
--- solo-project-based-on-jypfans/src/main/java/com/jarfans/solo/project/based/on/jypfans/post/PostingController.java
+++ solo-project-based-on-jypfans/src/main/java/com/jarfans/solo/project/based/on/jypfans/post/PostingController.java
@@ -1,5 +1,42 @@
 package com.jarfans.solo.project.based.on.jypfans.post;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/post")
 public class PostingController {
+
+  private final PostService postService;
+
+  @PostMapping("")
+  public void addPost(@NonNull @RequestBody Post postInfo){
+    postService.addPost(postInfo);
+  }
+
+  @GetMapping("")
+  public void findAll(){
+    postService.findAll();
+
+  }
+
+  @PatchMapping("/{id}")
+  public void update(){
+    postService.update();
+  }
+
+  @DeleteMapping("/{id}")
+  public void delete(){
+    postService.delete();
+  }
 
 }

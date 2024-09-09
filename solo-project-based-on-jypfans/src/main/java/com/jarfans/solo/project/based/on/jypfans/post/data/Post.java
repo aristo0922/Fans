955bc74818b4f8a7aeaf59506bff420ad2c766e0
aristo <dkfud2121@naver.com>
2024-09-09 19:25:05 +0900

    feat: post 객체  정리

--- solo-project-based-on-jypfans/src/main/java/com/jarfans/solo/project/based/on/jypfans/post/data/Post.java
+++ solo-project-based-on-jypfans/src/main/java/com/jarfans/solo/project/based/on/jypfans/post/data/Post.java
@@ -6,10 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Entity
 @Builder
@@ -20,17 +23,27 @@ import lombok.RequiredArgsConstructor;
 public class Post {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id")
+  @Column(name = "id", nullable = false, unique = true)
   private long id;
 
-  @Column(name = "team_id")
+  @Column(name = "team_id", nullable = false)
   private long teamId;
-  @Column(name = "fan_id")
+  @Column(name = "fan_id", nullable = false)
   private long fanId;
-  @Column(length = 100)
+  @Column(length = 100, nullable = false)
   private String subject;
-  @Column(length = 500)
+  @Column(length = 500, nullable = false)
   private String content;
+
+  @Column(name = "created_at")
+  private LocalDate createdAt;
+
+  @Column(name = "updated_at")
+  @LastModifiedDate
+  private LocalDate updatedAt;
+
+  @Column(name = "deleted_at")
+  private LocalDate deletedAt;
 
 //  public void setId(long id){
 //    this.id = id;
---
+++ solo-project-based-on-jypfans/src/main/java/com/jarfans/solo/project/based/on/jypfans/post/data/SavePostDTO.java
@@ -0,0 +1,17 @@
+package com.jarfans.solo.project.based.on.jypfans.post.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class SavePostDTO {
+  private String subject;
+  private String content;
+  private long fanId;
+  private long teamId;
+}
